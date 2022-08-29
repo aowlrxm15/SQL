d68f1c2b6338edf1733fa164440bbae3aad5fc88
aowlrxm15 <aowlrxm15@gmail.com>
2022-08-30 01:45:33 +0900

    추 가

--- SQL 연습문제 1.sql
+++ SQL 연습문제 1.sql
@@ -8,6 +8,7 @@ create user 'java2_admin1'@'%' identified by '1234';
 grant all privileges on `java2_shop`.* to 'java2_admin1'@'%';
 flush privileges;
 
+
 #실습 1-2
 create table `customer` (
 	`custid` varchar(10) primary key,  
@@ -72,19 +73,83 @@ select `custid`, `name`, `hp` from `customer`;
 
 #실습 1-6
 select * from `product`;
+
 #실습 1-7
+select `company` from `product`;
+
 #실습 1-8
+SELECT DISTINCT `company` FROM `product`;
+
 #실습 1-9
+select `product`, `price` from `product`;
+
 #실습 1-10
+select `prodname` , `price` as `조정단가` from `product`;
+
 #실습 1-11
+select `prodname`, `stock`, `price` from `product` 
+
 #실습 1-12
+select `orderproduct`, `ordercount`, `orderdate` from `order`
+
 #실습 1-13
+select `orderproduct`, `ordercount`, `orderdate` from `order` and
+
 #실습 1-14
+select * from `product` where `price` <= 2000;
+
 #실습 1-15
+select `custid`, `name`, `hp`, `addr` from `customer` where `name` like 김 _ _;
+
 #실습 1-16
+select `custid`, `name`, `hp`, `addr` from `customer` where `name` like 정_;
+
 #실습 1-17
+select * from `customer` where `hp` null;
+
 #실습 1-18
+select * from `customer` where `addr`
+
 #실습 1-19
+select * from `customer` order by `` desc;
+
 #실습 1-20
+select * from `order` where `ordercount` >= 3 order by `ordercount` desc;
+
 #실습 1-21
+select avg() as `단가평균` from `product`;
+
 #실습 1-22
+select sum() as `재고량 합계` from `product` where `company`='농심';
+
+#실습 1-23
+select as `고객수` from `customer`;
+
+#실습 1-24
+select count() as `제조업체 수` from `product`;
+
+#실습 1-25
+select `orderproduct` as `주문 상품번호`, sum(`ordercount`) as `총 주문수량` from `order`
+
+#실습 1-26
+select `company` as `제조업체`, count(*) as `제품수` , max(`price`) as `최고가` from `product`
+
+#실습 1-27
+select `company` as `제조업체`, count(*) as `제품수` , max(`price`) as `최고가` from `product` group by `company`
+
+#실습 1-28
+SELECT `orderProduct`, `orderId`, SUM(`orderCount`) AS `총 주문수량` FROM `Order`
+
+#실습 1-29
+select a.orderid, b.prodname from `order` as a 
+join 
+on
+where `orderid` = `c102`;
+
+#실습 1-30
+SELECT `orderid`, `name`, `prodName`, `orderDate` FROM `Order` AS a
+JOIN `Customer` AS b
+ON a.orderId = b.custId
+JOIN `Product` AS c
+ON a.orderProduct = c.prodNo
+WHERE 
